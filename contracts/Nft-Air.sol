10fef60cf9ccb5027d0110e54f840ad78f2c4ec3
Abhishek Tripathi <95579106+XavierOP877@users.noreply.github.com>
2022-11-02 19:29:33 +0530

    app update

--- contracts/Nft-Air.sol
+++ contracts/Nft-Air.sol
@@ -10,4 +10,41 @@ contract MemeForest is ReentrancyGuard{
     using Counters for Counters.Counter;
     Counters.Counter public NumOfAllMemes;
     Counters.Counter public NumOfAllMemebers;
+
+    struct MemeMembers{
+        string Name;
+        address MemberAddress;
+        uint MyId;
+        uint MyMemes;
+        uint MyStarredMemes;
+        uint MyDeletedMemes;
+        string Datejoined;
+    }
+
+    struct MemeFiles{
+        string Memeinfo;
+        address Owner;
+        uint fileId;
+        bool starred;
+        uint Stars;
+        uint Likes;
+        string DateOfCreation;
+        string FileType;
+        bool IsDownloadable;
+    }
+
+    mapping(uint => MemeMembers) private IdMembers;
+    mapping(address => bool) private alreadyAMember;
+    mapping(address => mapping(uint => bool)) private DidyouStar;
+    mapping(address => mapping(uint => bool)) private DidyouLike;
+    mapping(uint => MemeFiles) private IdMemeFiles;
+    mapping(uint => address) private StarredMemeFiles;
+
+    uint public NumberOfUploads;
+
+    event Memberjoined(
+        uint256 MemberId,
+        string MemberName,
+        s 
+    )
 }
