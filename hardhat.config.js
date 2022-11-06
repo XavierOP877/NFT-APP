f857f586d0c962cfbe177785191afb5fba8140d0
Abhishek Tripathi <95579106+XavierOP877@users.noreply.github.com>
2022-11-06 20:56:51 +0530

    m

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({path: ".env"});
+
+const ANKR_ID = process.env.ANKR_ID;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -7,5 +11,9 @@ module.exports = {
     hardhat:{
       chainId:1337
     },
-  }
+    mumbai:{
+      url:ANKR_ID,
+      accounts:[PRIVATE_KEY],
+    },
+  },
 };
